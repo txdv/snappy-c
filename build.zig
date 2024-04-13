e7eb485cea32cc8a9f6963d11addc128b8c6b2bc
Andrius Bentkus <andrius.bentkus@gmail.com>
2024-04-13 09:27:28 +0300

    build.zig with static library

---
+++ build.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const static_library = b.addStaticLibrary(.{
+        .name = "static_library",
+        .target = target,
+        .optimize = .ReleaseFast,
+    });
+    static_library.addCSourceFiles(.{
+        .files = &.{
+            "map.c",
+            "snappy.c",
+            "util.c",
+        },
+    });
+    b.installArtifact(static_library);
+}
